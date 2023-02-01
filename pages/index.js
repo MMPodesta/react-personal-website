ad5ff99813d88ee0bb18ebac4774407591c7a889
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-01 16:49:35 +0000

    Adding services

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import { BsFillMoonStarsFill } from 'react-icons/bs'
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 import Image from "next/image";
 import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -47,6 +50,82 @@ export default function Home() {
             <Image src={deved} layout='fill' objectFit='cover'/>
           </div>
         </section>
+        
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Services I offer</h3>
+            <p className='text-md py-2 leading-8 text-gray-80'>
+              I am a computer science student based in Dublin, 
+              Interested in full stack development and cyber security.
+            </p>
+            <p className='text-md py-2 leading-8 text-gray-800'>
+              I am a computer science student based in Dublin, 
+              Interested in full stack development and cyber security.
+              I am a computer science student based in 
+              <span className='text-teal-500'> Dublin</span>, 
+              Interested in full stack development and cyber security.
+            </p>
+          </div>
+          <div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={design} width={100} height={100} className='mx-auto'/>
+              <h3 className='text-lg font-medium pt-8 pb-2'>
+                Beatiful Designs
+              </h3>
+              <p className='py-2'>
+                Creating elegant designs suited for your project
+              </p>
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={code} width={100} height={100} className='mx-auto'/>
+              <h3 className='text-lg font-medium pt-8 pb-2'>
+                Beatiful Designs
+              </h3>
+              <p className='py-2'>
+                Creating elegant designs suited for your project
+              </p>
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+            </div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+              <Image src={consulting} width={100} height={100} className='mx-auto'/>
+              <h3 className='text-lg font-medium pt-8 pb-2'>
+                Beatiful Designs
+              </h3>
+              <p className='py-2'>
+                Creating elegant designs suited for your project
+              </p>
+              <h4 className='py-4 text-teal-600'>Design tools I use</h4>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+              <p className='text-gray-800 py-1'>Photoshop</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Portfolio</h3>
+            <p className='text-md py-2 leading-8 text-gray-80'>
+              I am a computer science student based in Dublin, 
+              Interested in full stack development and cyber security.
+            </p>
+            <p className='text-md py-2 leading-8 text-gray-800'>
+              I am a computer science student based in Dublin, 
+              Interested in full stack development and cyber security.
+              I am a computer science student based in 
+              <span className='text-teal-500'> Dublin</span>, 
+              Interested in full stack development and cyber security.
+            </p>
+          </div>
+        </section>
+
       </main>
     </>
   );
