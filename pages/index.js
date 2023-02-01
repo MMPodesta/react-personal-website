73cca4634994709a663924f5f3de5849526cf9e6
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-01 16:18:28 +0000

    Adding front page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
+import Image from "next/image";
+import deved from "../public/dev-ed-wave.png";
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -28,7 +31,21 @@ export default function Home() {
                rounded-md ml-8' href='#'>Resume</a></li>
             </ul>
           </nav>
-          
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium'>Mauricio murta</h2>
+            <h3 className='text-2xl py-2'>Developer and designer.</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>
+              I am a computer science student based in Dublin, 
+              Interested in full stack development and cyber security.
+            </p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillGithub/>
+            <AiFillLinkedin/>
+          </div>
+          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+            <Image src={deved} layout='fill' objectFit='cover'/>
+          </div>
         </section>
       </main>
     </>
