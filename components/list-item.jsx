0b562c1e3ca69b3aabb7a830c89768598e914ba4
Ahmed Saheed <ahmedsaheed2@outlook.com>
2023-02-02 22:21:49 +0000

    mkdir components & touch list-item.jsx & update index.js

---
+++ components/list-item.jsx
@@ -0,0 +1,11 @@
+export const ListItem = ({ children }) => {
+    return(
+        <>
+        {
+            children.map((child) => (
+                <p className='text-gray-800 py-1 dark:text-gray-400'>{child}</p>
+            )
+        )}
+        </>
+    )}
+
